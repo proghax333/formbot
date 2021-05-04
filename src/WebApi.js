27cc178fceadaba8d71826697d06ae182b41970b
Atmanand Nagpure <skillzworldtech@gmail.com>
2021-05-04 22:40:21 +0530

    Change url input in express api server

--- src/WebApi.js
+++ src/WebApi.js
@@ -10,16 +10,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify({ message: "Google Form Model Generator" }));
+  res.send(JSON.stringify({ message: 'Google Form Model Generator' }));
 });
 
-app.get('/api/model/:url', (req, res) => {
-  const url = req.params.url;
-  FormParser(url).then((parsed) => {
-    res.type('application/json').send(JSON.stringify(parsed));
-  })
+app.get('/api/model/', (req, res) => {
+  const url = req.query.url;
+  if (url) {
+    FormParser(url).then((parsed) => {
+      res.type('application/json').send(JSON.stringify(parsed));
+    });
+  } else {
+    res.status(404);
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Google Form Model Generator listening at http://localhost:${port}`);
+  console.log(
+    `Google Form Model Generator listening at http://localhost:${port}`
+  );
 });
